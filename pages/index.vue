0b62ae04af76c7f29299ba575f733df4d71d8e09
Egwuchukwu Stephen Diala <dialaegwuchukwu@gmail.com>
2023-06-24 22:33:45 +0100

    chore: test element

--- pages/index.vue
+++ pages/index.vue
@@ -3,28 +3,28 @@
 </script>
 
 <template>
-    <div class="p-4">
+    <Container outer-class="mt-9">
         <div class="max-w-sm rounded overflow-hidden shadow-lg mb-4 bg-white dark:bg-gray-900">
-        <img
-            class="w-full"
-            src="https://tailwindcss.com/img/card-top.jpg"
-            alt="Sunset in the mountains"
-        />
-        <div class="px-6 py-4 pb-2">
-            <div class="font-bold text-xl mb-2 dark:text-white">The Coldest Sunset</div>
-            <p class="text-gray-700 dark:text-gray-300">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus
-            quia, nulla! Maiores et perferendis eaque, exercitationem praesentium
-            nihil.
-            </p>
+            <img
+                class="w-full"
+                src="https://tailwindcss.com/img/card-top.jpg"
+                alt="Sunset in the mountains"
+            />
+            <div class="px-6 py-4 pb-2">
+                <div class="font-bold text-xl mb-2 dark:text-white">The Coldest Sunset</div>
+                <p class="text-gray-700 dark:text-gray-300">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus
+                quia, nulla! Maiores et perferendis eaque, exercitationem praesentium
+                nihil.
+                </p>
+            </div>
+            <div class="px-6 py-4">
+                <span class="badge mr-2">#photography</span>
+                <span class="badge mr-2">#travel</span>
+                <span class="badge">#winter</span>
+            </div>
         </div>
-        <div class="px-6 py-4">
-            <span class="badge mr-2">#photography</span>
-            <span class="badge mr-2">#travel</span>
-            <span class="badge">#winter</span>
-        </div>
-        </div>
-    </div>
+    </Container>
 </template>
 
 <style lang="postcss" scoped>
