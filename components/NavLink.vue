23e5ac881ede34d6c3bfdd94dc57b17a94f74c8f
Egwuchukwu Stephen Diala <dialaegwuchukwu@gmail.com>
2023-06-24 22:15:28 +0100

    feat: create navlink component

---
+++ components/NavLink.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+interface NavLinkProps {
+    href: string
+}
+
+defineProps<NavLinkProps>()
+</script>
+
+<template>
+    <NuxtLink :to="href" class="transition hover:text-teal-500 dark:hover:text-teal-400">
+      <slot/>
+    </NuxtLink>
+</template>
+
+<style scoped>
+
+</style>
