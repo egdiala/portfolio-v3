97bbf5d21d17ea7402a565fb8a2f6e9b2c7c0908
Egwuchukwu Stephen Diala <dialaegwuchukwu@gmail.com>
2023-06-24 22:13:42 +0100

    feat: create app structure

--- app.vue
+++ app.vue
@@ -3,10 +3,18 @@
 </script>
 
 <template>
-  <Body class="bg-zinc-50 dark:bg-black">
-    <Container>
-      <DesktopNavigation />
-    </Container>
-    <NuxtPage />
+  <Body class="flex h-full flex-col bg-zinc-50 dark:bg-black">
+    <div class="fixed inset-0 flex justify-center sm:px-8">
+      <div class="flex w-full max-w-7xl lg:px-8">
+        <div class="w-full bg-white ring-1 ring-zinc-100 dark:bg-zinc-900 dark:ring-zinc-300/20" />
+      </div>
+    </div>
+    <div class="relative">
+      <Header />
+      <main>
+        <NuxtPage />
+      </main>
+      <Footer />
+    </div>
   </Body>
 </template>
