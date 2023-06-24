e43a8e13cf53567bd95deb657ad6adc48b39090f
Egwuchukwu Stephen Diala <dialaegwuchukwu@gmail.com>
2023-06-24 22:15:17 +0100

    feat: create mode toggle component

---
+++ components/MobileNavItem.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+interface MobileNavItemProps {
+    href: string
+}
+defineProps<MobileNavItemProps>()
+</script>
+
+<template>
+    <li>
+      <HeadlessPopoverButton as="a" :href="href" class="block py-2">
+        <slot />
+      </HeadlessPopoverButton>
+    </li>
+</template>
+
+<style scoped>
+
+</style>
