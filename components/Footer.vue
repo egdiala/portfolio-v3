91996ac96043c802d2ebeca02bf4fc1a9cdca6cd
Egwuchukwu Stephen Diala <dialaegwuchukwu@gmail.com>
2023-06-24 22:14:41 +0100

    feat: create footer component

---
+++ components/Footer.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <footer class="mt-32">
+      <Container inner-class="border-t border-zinc-100 pb-16 pt-10 dark:border-zinc-700/40">
+        <div class="flex flex-col items-center justify-between gap-6 sm:flex-row">
+            <div class="flex flex-wrap justify-center gap-x-6 gap-y-1 text-sm font-medium text-zinc-800 dark:text-zinc-200">
+                <NavLink href="/about">About</NavLink>
+                <NavLink href="/work">Work</NavLink>
+                <NavLink href="/projects">Projects</NavLink>
+                <NavLink href="/stack">Stack</NavLink>
+            </div>
+            <p class="text-sm text-zinc-400 dark:text-zinc-500">
+            &copy; {{ new Date().getFullYear() }} Egwuchukwu S. Diala. All rights
+            reserved.
+            </p>
+        </div>
+      </Container>
+    </footer>
+</template>
+
+<style scoped>
+
+</style>
