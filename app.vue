3ac84f85c6710d7d9a82f003f7d487a8f2c024d1
Egwuchukwu Stephen Diala <dialaegwuchukwu@gmail.com>
2023-06-29 01:10:40 +0100

    feat: responsive

--- app.vue
+++ app.vue
@@ -25,7 +25,7 @@ function cursor(e: MouseEvent) {
 
 <template>
   <Body class="flex h-full flex-col bg-zinc-50 dark:bg-black">
-    <div ref="mouse" class="absolute w-[2rem] h-[2rem] z-[99] transform -translate-x-1/2 -translate-y-1/2 pointer-events-none rounded-full border-2 dark:border-white border-gray-600"/>
+    <div ref="mouse" class="absolute w-[2rem] hidden lg:flex h-[2rem] z-[99] transform -translate-x-1/2 -translate-y-1/2 pointer-events-none rounded-full border-2 dark:border-white border-gray-600"/>
     <div class="fixed inset-0 flex justify-center sm:px-8">
       <div class="flex w-full max-w-7xl lg:px-8">
         <div class="w-full bg-white ring-1 ring-zinc-100 dark:bg-zinc-900 dark:ring-zinc-300/20" />
@@ -43,7 +43,7 @@ function cursor(e: MouseEvent) {
 
 <style>
 * {
-  @apply cursor-none !important;
+  @apply lg:cursor-none !important;
 }
 
 .blur-me {
