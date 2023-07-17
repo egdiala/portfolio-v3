2904b38582b242d327589783adf77f08438867a4
Egwuchukwu Stephen Diala <dialaegwuchukwu@gmail.com>
2023-07-17 19:41:54 +0100

    feat: setup seo

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,14 +1,29 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      titleTemplate: '%siteName %titleSeparator %pageTitle'
+    }
+  },
   devtools: { enabled: true },
   pages: true,
+  extends: ["nuxt-seo-kit"],
+  runtimeConfig: {
+    public: {
+      siteUrl: process.env.NUXT_PUBLIC_SITE_URL || 'https://example.com',
+      siteName: 'Egwuchukwu S. Diala',
+      siteDescription: 'Egwuchukwu Diala\'s Portfolio Website',
+      language: 'en',
+      titleSeparator: '-'
+    }
+  },
   modules: [
     "@nuxtjs/tailwindcss",
     "@nuxtjs/color-mode",
     "nuxt-headlessui",
     "@nuxt/image",
     "nuxt-icon",
-    "nuxt-swiper"
+    "nuxt-swiper",
   ],
   colorMode: { classSuffix: '' },
   ssr: true
