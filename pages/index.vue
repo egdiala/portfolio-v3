50a2efad3f595ef9e9e5ed16ef1515d9c7edbc4b
Egwuchukwu Stephen Diala <dialaegwuchukwu@gmail.com>
2023-08-26 02:56:35 +0100

    fix: image url

--- pages/index.vue
+++ pages/index.vue
@@ -93,7 +93,7 @@ onMounted(() => {
         <div class="-my-4 flex justify-center gap-5 overflow-hidden py-11 sm:gap-8">
             <div v-for="(image, imageIndex) in images" :key="imageIndex" v-motion :initial="{ opacity: 0, scale: 0, rotate: 0 }" :enter="{ opacity: 1, scale: 1, rotate: rotations[imageIndex % rotations.length], transition:{ type: 'tween', ease: 'easeOut', duration: 30, delay: (200 * (imageIndex+1)) } }"
             class="relative aspect-[9/10] transform hover:sm:-translate-y-10 hover:-translate-y-3 transition-all ease duration-300 w-44 flex-none overflow-hidden rounded-xl bg-zinc-100 dark:bg-zinc-800 sm:w-72 sm:rounded-2xl">
-                <NuxtImg :src="image" :alt="`image_${imageIndex}`" sizes="sm:640px" class="absolute inset-0 h-full w-full object-cover" />
+                <NuxtImg :src="image.replace('/_ipx/w_640', '')" :alt="`image_${imageIndex}`" sizes="sm:640px" class="absolute inset-0 h-full w-full object-cover" />
             </div>
           </div>
     </div>
